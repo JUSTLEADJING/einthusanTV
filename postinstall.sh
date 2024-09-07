3a7647adce778528642c7315c7e828ee8898c54a
JUSTLEADJING <bman12285@gmail.com>
2024-09-08 01:45:52 +0800

    Update postinstall.sh

--- postinstall.sh
+++ postinstall.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-curl --location --output beamup-logger https://github.com/dexter21767/beamup-logger/releases/latest/download/beamup-logger-node16-linux
+curl --location --output beamup-logger https://github.com/JUSTLEADJING/beamup-logger/releases/latest/download/beamup-logger-node16-linux
 chmod +x beamup-logger
